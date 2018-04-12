d34c4fd2388d379a41247ebba0de95f0c8be16c4
YKGDWADE3 <superwadeg3@gmail.com>
2018-04-13 01:50:44 +0800

    Add MyMapTest

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -2,8 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class MyMap {
 
@@ -17,22 +16,68 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        if (array == null) {
+            return null;
+        }
+        ArrayList<Integer> resultList = new ArrayList<>();
+        for (int i = 0; i < array.size(); i++) {
+            resultList.add(array.get(i) * 3);
+        }
+        return resultList;
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        if (array == null) {
+            return null;
+        }
+        ArrayList<String> resultList = new ArrayList<>();
+        for (int i = 0; i < array.size(); i++) {
+            resultList.add(letterList.get((array.get(i)-1) % 26));
+        }
+        return resultList;
     }
 
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+        if (array == null) {
+            return null;
+        }
+        ArrayList<String> resultList = new ArrayList<>();
+        for (int i = 0; i < array.size(); i++) {
+            resultList.add(mapLetters(array.get(i)));
+        }
+        return resultList;
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        if (array == null) {
+            return null;
+        }
+        ArrayList<Integer> resultList = new ArrayList<>(array);
+        Collections.sort(resultList, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 >= o2 ? -1 : 1;
+            }
+        });
+        return resultList;
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        if (array == null) {
+            return null;
+        }
+        ArrayList<Integer> resultList = new ArrayList<>(array);
+        Collections.sort(resultList);
+        return resultList;
+    }
+
+    public String mapLetters(int number){
+        StringBuffer stringBuffer = new StringBuffer();
+        int index;
+        while (number > 0) {
+            stringBuffer.append(letterList.get((--number) % 26));
+            number /= 26;
+        }
+        return stringBuffer.reverse().toString();
     }
 }
