4ca3bda302215a19b7649eca92fa59aee32d94a2
YKGDWADE3 <superwadeg3@gmail.com>
2018-04-13 01:03:06 +0800

    Add FlatenTest

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,6 +2,8 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 public class Flaten {
@@ -12,10 +14,23 @@ public class Flaten {
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        if (array == null) {
+            return null;
+        }
+        ArrayList<Integer> resultList = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                resultList.add(array[i][j]);
+            }
+        }
+        return resultList;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        ArrayList<Integer> oneDimList = (ArrayList<Integer>) transformToOneDimesional();
+        LinkedHashSet<Integer> linkedHashSet = new LinkedHashSet<>(oneDimList);
+        oneDimList.clear();
+        oneDimList.addAll(linkedHashSet);
+        return oneDimList;
     }
 }
